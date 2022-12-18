4d8482fd3a31ede88d51f6e49ae4b8a197fc1674
RebelWright <rebeltrentwrightjr@gmail.com>
2022-12-18 14:07:08 -0600

    update methods for reimb_status dao and interface 2:06pm Sun

--- ers_p1/src/main/java/com/revature/dao/ERS_Reimb_StatusDAO.java
+++ ers_p1/src/main/java/com/revature/dao/ERS_Reimb_StatusDAO.java
@@ -1,4 +1,62 @@
 package com.revature.dao;
 
-public class ERS_Reimb_StatusDAO {
+import com.revature.utils.ConnectionUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ERS_Reimb_StatusDAO implements ERS_Reimb_StatusDAO_Interface{
+
+    @Override
+    public boolean approveReimb_Status(int reimb_status, int reimb_id) {
+        try(Connection conn = ConnectionUtil.getConnection()){
+
+            //create our SQL String (to be filled with values from the method arguments)
+            String sql = "update ers_reimbursements set reimb_status_fk = 2 where role_title = ?;";
+
+            //Prepared statement so that we can fill appropriate values
+            PreparedStatement ps = conn.prepareStatement(sql);
+
+            //using ps.setXZY we can fill the wildcards (?) in our SQL statement
+            ps.setInt(1, reimb_id);
+
+            //execute the update!
+            ps.executeUpdate();
+
+            //if we get this far in the try block, we can assume nothing went wrong. return true.
+            return true;
+
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean denyReimb_Status(int reimb_status, int reimb_id) {
+        try(Connection conn = ConnectionUtil.getConnection()){
+
+            //create our SQL String (to be filled with values from the method arguments)
+            String sql = "update ers_reimbursements set reimb_status_fk = 3 where reimb_id = ?;";
+
+            //Prepared statement so that we can fill appropriate values
+            PreparedStatement ps = conn.prepareStatement(sql);
+
+
+            ps.setInt(1, reimb_id);
+
+            //execute the update!
+            ps.executeUpdate();
+
+            //if we get this far in the try block, we can assume nothing went wrong. return true.
+            return true;
+
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        return false;
+    }
 }
--- ers_p1/src/main/java/com/revature/dao/ERS_Reimb_StatusDAO_Interface.java
+++ ers_p1/src/main/java/com/revature/dao/ERS_Reimb_StatusDAO_Interface.java
@@ -1,4 +1,6 @@
 package com.revature.dao;
 
 public interface ERS_Reimb_StatusDAO_Interface {
+    boolean approveReimb_Status(int reimb_status, int reimb_id);
+    boolean denyReimb_Status(int reimb_status, int reimb_id);
 }
