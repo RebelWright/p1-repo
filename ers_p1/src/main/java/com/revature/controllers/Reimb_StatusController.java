0a690a4b3fd09aee734fe5dbe607d0c309f8f991
RebelWright <rebeltrentwrightjr@gmail.com>
2022-12-21 09:37:55 -0600

    reimbStatusController update 9:37am Wed

--- ers_p1/src/main/java/com/revature/controllers/Reimb_StatusController.java
+++ ers_p1/src/main/java/com/revature/controllers/Reimb_StatusController.java
@@ -21,7 +21,7 @@ public class Reimb_StatusController {
         /*To access a path parameter, we can use ctx.pathParam()
         the value that the user includes will be stored in the "title" key
         in this case, our Launcher endpoint handler calls it "title", so we need to call "title" here */
-        int reimb_id = Integer.parseInt(ctx.pathParam("reimb_id"));
+        int reimb_id = Integer.parseInt(ctx.pathParam("reimb_status_fk"));
 
         //int to hold the new Role salary that the user will input in the HTTP Request body
         //int reimb_status_id = Integer.parseInt(ctx.body());
